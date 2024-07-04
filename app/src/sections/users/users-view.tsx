016ab95c725506c0cff911509c48fabd17ee3377
guilhermelim <guilhermem.lima@outlook.com>
2024-07-04 08:00:46 -0300

    add title and subtitle to user session

--- app/src/sections/users/users-view.tsx
+++ app/src/sections/users/users-view.tsx
@@ -1,7 +1,7 @@
 import CreateUser from "./view/create-user";
 import ChartsPie from "./view/charts-pie";
 import ListUser from "./view/list-user";
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 import EditUser from "./view/edit-user";
 import { UsersProvider } from "./context";
@@ -20,6 +20,20 @@ export default function UsersView() {
             alignItems: "center",
           }}
         >
+          <Typography variant="h3" component="h1" sx={{ mb: 2 }}>
+            Fullstack Challenge
+          </Typography>
+
+          <Typography
+            variant="subtitle1"
+            component="body"
+            align="center"
+            sx={{ mb: 2 }}
+          >
+            This is a project that offers a quick start with a GraphQL API and
+            an integrated front-end.
+          </Typography>
+
           <Stack
             direction="row"
             justifyContent="center"
